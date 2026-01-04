93f10af527edeeee0304a16a36503e0be863090f
dusan-mrsic <mrsic.dusan@gmail.com>
2026-01-04 11:49:56 +0100

    example 08 - arrays

---
+++ 08-arrays.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [5]int
+	fmt.Println("emp:", a)
+
+	a[4] = 100
+	fmt.Println("set:", a)
+	fmt.Println("get:", a[4])
+
+	fmt.Println("len:", len(a))
+
+	b := [5]int{1, 2, 3, 4, 5}
+	fmt.Println("dec:", b)
+
+	b = [...]int{100, 3: 400, 500}
+	fmt.Println("idx:", b)
+
+	var twoD = [2][3]int{
+		{1, 2, 3},
+		{1, 2, 3},
+	}
+	fmt.Println("2d: ", twoD)
+}
