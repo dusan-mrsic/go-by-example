52beca767fc76c678af7ef7059eea7f94a51f3f7
dusan-mrsic <mrsic.dusan@gmail.com>
2026-01-08 22:30:07 +0100

    example 22 - enums

---
+++ 22-enums.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+type ServerState int
+
+const (
+	StateIdle ServerState = iota
+	StateConnected
+	StateError
+	StateRetrying
+)
+
+var stateName = map[ServerState]string{
+	StateIdle:      "idle",
+	StateConnected: "connected",
+	StateError:     "error",
+	StateRetrying:  "retrying",
+}
+
+func (ss ServerState) String() string {
+	return stateName[ss]
+}
+
+func main() {
+	ns := transition(StateIdle)
+	fmt.Println(ns)
+	ns = transition(ns)
+	fmt.Println(ns)
+}
+
+func transition(ss ServerState) ServerState {
+	switch ss {
+	case StateIdle:
+		return StateConnected
+	case StateConnected, StateRetrying:
+		return StateIdle
+	case StateError:
+		return StateError
+	default:
+		panic(fmt.Errorf("unknow state: %s", ss))
+	}
+}
